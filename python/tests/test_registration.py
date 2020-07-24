deb4d92c0d38691e86af5c9ccc1e4f05cf467b95
kanan132 <63307356+kanan132@users.noreply.github.com>
2020-07-24 12:47:44 +0400

    Add files via upload

--- python/tests/test_registration.py
+++ python/tests/test_registration.py
@@ -5,14 +5,22 @@
 
 import os
 import unittest
+from stories import RegisterUser
+from models import User
 
 class TestPhoneBook(unittest.TestCase):
 
     def setUp(self):
-        pass
+        self.user1 = User(email="John@example.com", password="122345")
+        self.user2 = User(email="harvey@example.com", password="1456845")
+        self.user3 = User(email="", password="1456845")
+        self.user4 = User(email="harvey@example.com", password="")
+        self.user5 = User(email="", password="")
+        self.rg = RegisterUser()
 
     def test_can_find_empty_email_pass(self):
-        pass
+        self.assertEqual("",self.user5.email)
+        self.assertEqual("",self.user4.password)
 
     def test_can_make_user_use_strong_pass(self):
         pass
